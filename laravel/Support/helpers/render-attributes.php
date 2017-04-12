343e8ff062a61370f1967d6155152910988fc970
Tanner Hodges <austintannerhodges@gmail.com>
2017-04-12 14:33:23 -0400

    Docblock + simplify render_attributes helper

--- laravel/Support/helpers/render-attributes.php
+++ laravel/Support/helpers/render-attributes.php
@@ -1,24 +1,24 @@
 <?php
 
 if (!function_exists('render_attributes')) {
-    function render_attributes(array $attrs, $prefix = '') {
 
+    /**
+     * Get string from array of HTML attributes.
+     * @param  array   $attrs
+     * @param  string  $prefix
+     * @return string
+     */
+    function render_attributes(array $attrs, $prefix = '')
+    {
         if (empty($attrs)) {
             return '';
         }
 
-        $keys = array_keys($attrs);
-
         $attrStrings = [];
-        foreach($keys as $key) {
-            $attrStrings[] = sprintf('%s%s="%s"', $prefix, $key, addcslashes($attrs[$key], "\""));
+        foreach(array_keys($attrs) as $key) {
+            $attrStrings[] = sprintf('%s%s="%s"', $prefix, $key, addcslashes($attrs[$key], '"'));
         }
 
-        // add an empty space before the full set of attributes
-        if (!empty($attrStrings)) {
-            array_unshift($attrStrings, "");
-        }
-
-        return implode(" ", $attrStrings);
+        return ' ' . implode(' ', $attrStrings);
     }
 }
