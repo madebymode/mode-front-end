1b1034be796919fdc35319f061127608856d3aab
Tanner Hodges <austintannerhodges@gmail.com>
2017-04-12 14:34:03 -0400

    Docblock + simplify asset_url helper

--- laravel/Support/helpers/asset-url.php
+++ laravel/Support/helpers/asset-url.php
@@ -1,56 +1,74 @@
 <?php
+
 if (!function_exists('asset_url')) {
 
+// TODO: Either remove dependence on Laravel helpers or include core fallbacks:
+// - env
+// - elixir
+// - base_path
+// - public_path
+
     /**
-     * build web accessible asset path for a given asset. It will check
-     * for an elixir manifest file and attempt to load the asset from there.
-     * If one is not available, it will fallback to using a timestamp for cache
-     * busting if the ASSET_CACHE_BUSTING environment variable is set to true. Additionally,
-     * it will prepend a CDN hostname if one is defined using the ASSET_CDN_DOMAIN variable
+     * Build a web access path for a given asset based on the environment.
      *
-     * build a web access asset path depending on the environment.
-     * in production, this function will use the build in elixir()
-     * helper that uses asset versioning.
+     * 1. Elixir: Check for an [Elixir](https://laravel.com/docs/5.3/elixir) manifest file
+     *    and attempt to load the asset from there.
+     * 2. Cache Busting: Check for an `ASSET_CACHE_BUSTING` environment variable. If set to true,
+     *    add a timestamp to the asset for cache busting.
+     * 3. CDN: Check for an `ASSET_CDN_DOMAIN` environment variable. If set to true,
+     *    prepend the CDN hostname to the asset.
+     *
+     * @param  string  $src
+     * @param  string  $buildDirectory
+     * @return string
      */
-    function asset_url($src, $buildDirectory = 'build') {
-
-        static $cachets = null;
-
-        // check if asset cache busting is enabled. This is primarily done because cache busting
-        // requires rewrites and the .htaccess or server level. enabling this without the rewrites
-        // would result in a 404 for any assets that use this helper
-        if (env('ASSET_CACHE_BUSTING')) {
-            if (is_null($cachets)) {
-                // set a default cache timestamp in case a deployment timestamp isn't available
-                $cachets = time();
-                $deploytsFile = base_path('.deployts');
-                if (file_exists($deploytsFile)) {
-                    $cachets = trim(file_get_contents($deploytsFile));
-                }
-            }
-        }
-
-        $cdnDomain = rtrim(env('ASSET_CDN_DOMAIN', '/'), '/');
-
+    function asset_url($src, $buildDirectory = 'build')
+    {
         $path = ltrim(str_replace(public_path(), '', public_path($src)), '/');
 
-        // attempt to load the file from the asset manifest from elixir.
-        // prevent any errors if the asset isn't found.
-        $foundInManifest = false;
+        $found_in_manifest = false;
+
+        /**
+         * 1. Elixir
+         */
         if (file_exists(public_path($buildDirectory . '/rev-manifest.json'))) {
             try {
                 $path = elixir($src);
-                $foundInManifest = true;
-            } catch (\InvalidArgumentException $e) {
+                $found_in_manifest = true;
+            } catch (\InvalidArgumentException $e) {}
+        }
+
+        /**
+         * 2. Cache Busting
+         *
+         * @note This requires rewrite rules at the `.htaccess` or server level.
+         *    Enabling this without the rewrites will result in a 404 for any
+         *    assets that use this helper.
+         */
+        static $cache_ts = null;
+
+        if (env('ASSET_CACHE_BUSTING') && is_null($cache_ts)) {
+            // Default cache timestamp
+            $cache_ts = time();
+
+            // Get timestamp from deploy file if it exists
+            $deploy_ts_file = base_path('.deployts');
+            if (file_exists($deploy_ts_file)) {
+                $cache_ts = trim(file_get_contents($deploy_ts_file));
             }
         }
 
-        // append a cache busting timestamp if the asset wasn't found in a manifest
-        if (!$foundInManifest && !empty($cachets)) {
-            $fileExtension = pathinfo($path, PATHINFO_EXTENSION);
-            $path = preg_replace('/(' . preg_quote($fileExtension) . ')$/', $cachets . '.$1', $path);
+        // Only append timestamp if asset wasn't found in a manifest (Elixir appends its own timestamp)
+        if (!$found_in_manifest && !empty($cache_ts)) {
+            $file_extension = pathinfo($path, PATHINFO_EXTENSION);
+            $path = preg_replace('/(' . preg_quote($file_extension) . ')$/', $cache_ts . '.$1', $path);
         }
 
-        return $cdnDomain . '/' . ltrim($path, '/');
+        /**
+         * 3. CDN
+         */
+        $cdn_domain = rtrim(env('ASSET_CDN_DOMAIN', '/'), '/');
+
+        return $cdn_domain . '/' . ltrim($path, '/');
     }
 }
