9206ffd616bef58331cbafd2bc7de0efec6a8e26
Tanner Hodges <austintannerhodges@gmail.com>
2017-08-28 11:25:52 -0400

    Browsers: Add missing chrome helper references

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ The format is based on [Keep a Changelog](http://keepachangelog.com) and this pr
 
 ## [Unreleased]
 
+### Added
+
+- Add `chrome.getVersion()` helper
+
 ## [2.6.3] - 2017-08-10
 
 ### Added
--- resources/assets/js/browserDetect/index.js
+++ resources/assets/js/browserDetect/index.js
@@ -1,5 +1,6 @@
 module.exports = (function() {
   return {
+    chrome: require('./chrome'),
     fontFeatures: require('./fontFeatures'),
     internetExplorer: require('./internetExplorer'),
     ios: require('./ios')
