c67d4ba387988ae39c9492b0caba371e85b45d94
Tanner Hodges <austintannerhodges@gmail.com>
2017-09-21 11:22:37 -0400

    Touch helper: Add class to document instead of body
    Keep class-adding conventions the same between all browserDetect modules.

--- resources/assets/js/browserDetect/hasTouched.js
+++ resources/assets/js/browserDetect/hasTouched.js
@@ -1,12 +1,21 @@
 module.exports = (function(window, document, undefined) {
 
+  // TODO: Move this to a separate helper and reuse between browserDetect modules.
+  /**
+   * Add helper classes to document.
+   * @return {Boolean}
+   */
+  function addClassToDocument(className) {
+    return (document.documentElement.className = document.documentElement.className + ' ' + className);
+  }
+
   // The only way to detect touch with JavaScript
   // https://medium.com/@david.gilbertson/the-only-way-to-detect-touch-with-javascript-7791a3346685
   var hasTouched = false;
 
   window.addEventListener('touchstart', function onFirstTouch() {
     hasTouched = true;
-    document.body.classList.add('has-touched');
+    addClassToDocument('has-touched');
     window.removeEventListener('touchstart', onFirstTouch, false);
   }, false);
 
