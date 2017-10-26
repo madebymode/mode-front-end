ae5a230c15991a81e5911e4965088beef29a982c
Chris Gutierrez <cdotgutierrez@gmail.com>
2017-10-26 14:19:04 -0400

    update comments

--- laravel/Support/helpers/meta.php
+++ laravel/Support/helpers/meta.php
@@ -6,23 +6,30 @@ if (!function_exists('meta')) {
         protected $entries = [];
 
         /**
-         * add a meta data entry
+         * Add a meta data entry. Optionally override existing meta data entries.
+         *
          * @param string  $name       name attribute for the meta tag
          * @param string  $content    content attribute for the meta tag
-         * @param boolean $http_equiv boolean for the http-equiv attribute
+         * @param boolean $httpEquiv  boolean for the http-equiv attribute
          * @param boolean $override   true to override a previously set meta data item
          * @param array   $attrs      additional attrs to be placed on the meta tag
          */
-        public function add($name, $content, $http_equiv = false, $override = false, $attrs = []) {
+        public function add($name, $content, $httpEquiv = false, $override = false, $attrs = []) {
 
             if (!empty($this->entries[$name]) && !$override) {
                 return $this;
             }
 
-            $this->entries[$name] = ['content' => $content, 'http-equiv' => $http_equiv];
+            $this->entries[$name] = ['content' => $content, 'http-equiv' => $httpEquiv];
             return $this;
         }
 
+        /**
+         * Return a string of the meta tags for display in HTML
+         *
+         * @param  array  $default Array of meta data to use as the default. e.g: ['description' => ['content' => 'Page Description']]
+         * @return string          String of meta tags
+         */
         public function render($default = []) {
 
             $entries = array_merge((array) $default, (array) $this->entries);
