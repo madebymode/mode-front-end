562be49a61e572b761580c16082db895d859acf2
Chris Gutierrez <cdotgutierrez@gmail.com>
2017-11-10 12:58:52 -0500

    remove php/laravel files

--- laravel/Support/helpers.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once 'helpers/asset-url.php';
-require_once 'helpers/fix-widows.php';
-require_once 'helpers/icon.php';
-require_once 'helpers/render-attributes.php';
-require_once 'helpers/title.php';
-require_once 'helpers/meta.php';
