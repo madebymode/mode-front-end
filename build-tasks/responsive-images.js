2c2a605e252cfd0efb9f761c16c89fe95f7ceb83
Chris Gutierrez <cdotgutierrez@gmail.com>
2021-02-05 16:22:47 -0500

    updated responsive image generation to use promises

--- build-tasks/responsive-images.js
+++ build-tasks/responsive-images.js
@@ -3,9 +3,14 @@ const glob = require('glob');
 const path = require('path')
 const fs = require('fs')
 
+const { promisify } = require('util')
+
+const fileGlob = promisify(glob)
+const mkdir = promisify(fs.mkdir)
+
 module.exports = function(sourceConfig, options = {}) {
 
-  sharp.concurrency(6);
+  // sharp.concurrency(6);
 
   let sizeGlobs = Object.keys(sourceConfig)
 
@@ -16,40 +21,49 @@ module.exports = function(sourceConfig, options = {}) {
   }
 
   let checkedDirs = [];
-  sizeGlobs.forEach((globKey) => {
+  return Promise.allSettled(
+    sizeGlobs.map((globKey) => {
 
-    let sizes = sourceConfig[globKey]
+      let sizes = sourceConfig[globKey]
+      let sourceBaseDirectory = options.sourceDir
+      let destinationBaseDirectory = options.destDir
 
-    let sourceBaseDirectory = options.sourceDir
-    let destinationBaseDirectory = options.destDir
+      let sourceGlob = path.join(sourceBaseDirectory, globKey)
 
-    let sourceGlob = path.join(sourceBaseDirectory, globKey)
+      return fileGlob(sourceGlob, {})
+        .then((files) => {
+          return Promise.allSettled(
 
-    glob(sourceGlob, {}, function(err, files) {
+            files.map((sourceFile) => {
 
-      files.forEach((sourceFile) => {
+              // Parse the different parts of the file to compile the destination file path
+              let sourceDirname = path.dirname(sourceFile)
+              let sourceExtension = path.extname(sourceFile)
+              let sourceBasename = path.basename(sourceFile, sourceExtension);
 
-        // Parse the different parts of the file to compile the destination file path
-        let sourceDirname = path.dirname(sourceFile)
-        let sourceExtension = path.extname(sourceFile)
-        let sourceBasename = path.basename(sourceFile, sourceExtension);
+              // Get the full destination path for the file removing the source base directory
+              let destinationDirectory = path.join(destinationBaseDirectory, sourceDirname.replace(sourceBaseDirectory, ''))
 
-        let destinationDirectory = path.join(destinationBaseDirectory, sourceDirname.replace(sourceBaseDirectory, ''))
+              return mkdir(destinationDirectory, { recursive: true })
+                .then(() => {
+                  return Promise.allSettled(
+                    sizes.map((size) => {
+                      let destinationFile = path.join(destinationDirectory, `${sourceBasename}${size.rename.suffix}${sourceExtension}`)
 
-        // Ensure the destination directory exists
-        fs.mkdir(destinationDirectory, { recursive: true }, (err) => {
-          if (err) {
-            return console.warn(err);
-          }
-
-          // Generate the size for the current width
-          sizes.forEach((size) => {
-            sharp(sourceFile)
-              .resize(size.width, null)
-              .toFile(path.join(destinationDirectory, `${sourceBasename}${size.rename.suffix}${sourceExtension}`));
-          })
-        });
-      })
+                       return sharp(sourceFile)
+                         .resize(size.width, null)
+                         .toFile(destinationFile)
+                         .then((info) => {
+                           info.file = destinationFile
+                           return info
+                         })
+                         // .then((info) => console.log(info))
+                    })
+                  )
+                })
+             })
+          )
+        })
     })
-  })
+  )
 }
